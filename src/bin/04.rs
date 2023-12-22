803eb063befe5d449388d15295b475c114fa68bb
Koen Klaren <koen.klaren@gmail.com>
2023-12-22 13:53:56 +0100

    Day 4, clean-up

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -4,7 +4,7 @@ use nom::combinator::map_res;
 use nom::multi::separated_list0;
 use nom::sequence::{preceded, separated_pair, tuple};
 use nom::IResult;
-use std::collections::HashSet;
+use std::cmp::Ordering;
 
 advent_of_code::solution!(4);
 
@@ -26,20 +26,36 @@ fn parse_number_list(input: &str) -> IResult<&str, Vec<u32>> {
     separated_list0(space1, map_res(digit1, str::parse))(input)
 }
 
+fn count_wins(mut winning_numbers: Vec<u32>, mut numbers: Vec<u32>) -> usize {
+    winning_numbers.sort();
+    numbers.sort();
+
+    let mut result = 0;
+    let mut i = 0;
+    let mut j = 0;
+    while i < winning_numbers.len() && j < numbers.len() {
+        match winning_numbers[i].cmp(&numbers[j]) {
+            Ordering::Less => i += 1,
+            Ordering::Greater => j += 1,
+            Ordering::Equal => {
+                result += 1;
+                i += 1;
+                j += 1;
+            }
+        }
+    }
+    result
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let (_, cards) = parse_input(input).ok()?;
 
-    let mut result = 0;
-
-    for (winning_numbers, numbers) in cards {
-        let winning_numbers = HashSet::<u32>::from_iter(winning_numbers);
-        let numbers = HashSet::<u32>::from_iter(numbers);
-
-        let num_winners = winning_numbers.intersection(&numbers).count();
-        if num_winners > 0 {
-            result += 2u32.pow(num_winners as u32 - 1);
-        }
-    }
+    let result = cards
+        .into_iter()
+        .map(|(winning_numbers, numbers)| count_wins(winning_numbers, numbers))
+        .filter(|&num_winners| num_winners > 0)
+        .map(|num_winners| 2u32.pow(num_winners as u32 - 1))
+        .sum();
 
     Some(result)
 }
@@ -49,11 +65,7 @@ pub fn part_two(input: &str) -> Option<u32> {
 
     let original_card_wins = cards
         .into_iter()
-        .map(|(winning_numbers, numbers)| {
-            let winning_numbers = HashSet::<u32>::from_iter(winning_numbers);
-            let numbers = HashSet::<u32>::from_iter(numbers);
-            winning_numbers.intersection(&numbers).count()
-        })
+        .map(|(winning_numbers, numbers)| count_wins(winning_numbers, numbers))
         .collect::<Vec<_>>();
 
     let mut num_cards = vec![1; original_card_wins.len()];
