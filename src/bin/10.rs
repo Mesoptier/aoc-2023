c1bf73ac683dbd28e7924b1e48eae31439d5b404
Koen Klaren <koen.klaren@gmail.com>
2023-12-26 08:44:32 +0100

    Day 10, part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,6 +1,7 @@
+use itertools::Itertools;
 advent_of_code::solution!(10);
 
-#[derive(Eq, PartialEq, Copy, Clone)]
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
 enum Direction {
     North,
     South,
@@ -96,15 +97,15 @@ impl Map {
     }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let map = Map::from_str(input);
+fn both_parts(input: &str) -> (Option<u32>, Option<u32>) {
+    let mut map = Map::from_str(input);
 
     let (mut x, mut y) = map.start;
 
     // Find valid initial direction
-    let mut direction = Direction::VALUES
+    let start_directions = Direction::VALUES
         .into_iter()
-        .find(|direction| {
+        .filter(|direction| {
             let (nx, ny) = match direction {
                 Direction::North => {
                     if y == 0 {
@@ -134,10 +135,40 @@ pub fn part_one(input: &str) -> Option<u32> {
 
             map.tiles[ny][nx].step_from(*direction).is_some()
         })
+        .collect_tuple::<(_, _)>()
         .unwrap();
 
+    // Replace start tile with pipe
+    match start_directions {
+        (Direction::North, Direction::South) => {
+            map.tiles[y][x] = Tile::VerticalPipe;
+        }
+        (Direction::East, Direction::West) => {
+            map.tiles[y][x] = Tile::HorizontalPipe;
+        }
+        (Direction::North, Direction::East) => {
+            map.tiles[y][x] = Tile::NorthEastPipe;
+        }
+        (Direction::North, Direction::West) => {
+            map.tiles[y][x] = Tile::NorthWestPipe;
+        }
+        (Direction::South, Direction::East) => {
+            map.tiles[y][x] = Tile::SouthEastPipe;
+        }
+        (Direction::South, Direction::West) => {
+            map.tiles[y][x] = Tile::SouthWestPipe;
+        }
+        _ => unreachable!("Invalid start"),
+    }
+
+    let map = map;
+    let mut direction = start_directions.0;
+
+    let mut is_tile_on_loop = vec![vec![false; map.tiles[0].len()]; map.tiles.len()];
     let mut steps = 0;
     loop {
+        is_tile_on_loop[y][x] = true;
+
         (x, y) = match direction {
             Direction::North => (x, y - 1),
             Direction::South => (x, y + 1),
@@ -147,7 +178,7 @@ pub fn part_one(input: &str) -> Option<u32> {
         steps += 1;
 
         if x == map.start.0 && y == map.start.1 {
-            return Some(steps / 2);
+            break;
         }
 
         match map.tiles[y][x].step_from(direction) {
@@ -155,10 +186,66 @@ pub fn part_one(input: &str) -> Option<u32> {
             None => unreachable!("Invalid loop"),
         }
     }
+
+    let mut covered_tiles = 0;
+
+    for (y, row) in map.tiles.iter().enumerate() {
+        let mut is_within_loop = false;
+        let mut x = 0;
+
+        while x < row.len() {
+            if !is_tile_on_loop[y][x] {
+                if is_within_loop {
+                    covered_tiles += 1;
+                }
+                x += 1;
+                continue;
+            }
+
+            let tile = row[x];
+            match tile {
+                Tile::VerticalPipe => {
+                    covered_tiles += 1;
+                    is_within_loop = !is_within_loop;
+                }
+                Tile::NorthEastPipe | Tile::SouthEastPipe => {
+                    covered_tiles += 1;
+
+                    // Walk east until we hit the next corner
+                    while row[x] != Tile::NorthWestPipe && row[x] != Tile::SouthWestPipe {
+                        covered_tiles += 1;
+                        x += 1;
+                    }
+
+                    match (tile, row[x]) {
+                        (Tile::NorthEastPipe, Tile::SouthWestPipe) => {
+                            is_within_loop = !is_within_loop;
+                        }
+                        (Tile::SouthEastPipe, Tile::NorthWestPipe) => {
+                            is_within_loop = !is_within_loop;
+                        }
+                        _ => {}
+                    }
+                }
+                _ => unreachable!(),
+            }
+
+            x += 1;
+        }
+    }
+
+    let part_one = steps / 2;
+    let part_two = covered_tiles - steps;
+
+    (Some(part_one), Some(part_two))
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    both_parts(input).0
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    both_parts(input).1
 }
 
 #[cfg(test)]
@@ -180,7 +267,19 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 3,
+        ));
+        assert_eq!(result, Some(4));
+
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 4,
+        ));
+        assert_eq!(result, Some(8));
+
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 5,
+        ));
+        assert_eq!(result, Some(10));
     }
 }
