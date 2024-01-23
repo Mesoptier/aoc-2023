4346e739a507a5591d82aad5f6a4da72637193c9
Koen Klaren <koen.klaren@gmail.com>
2024-01-23 19:53:47 +0100

    Day 7: Precompute card value and hand type

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -16,16 +16,25 @@ fn parse_input(input: &str) -> IResult<&str, Vec<(&str, u32)>> {
     )(input)
 }
 
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-struct Card(char);
+#[derive(Copy, Clone, Eq, PartialEq)]
+enum JCardType {
+    Jack,
+    Joker,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Ord, PartialOrd)]
+struct Card(u32);
 
 impl Card {
-    fn value(&self) -> u32 {
-        match self.0 {
+    fn new(c: char, j_card_type: JCardType) -> Self {
+        Self(match c {
             'A' => 14,
             'K' => 13,
             'Q' => 12,
-            'J' => 11,
+            'J' => match j_card_type {
+                JCardType::Jack => 11,
+                JCardType::Joker => 1,
+            },
             'T' => 10,
             '9' => 9,
             '8' => 8,
@@ -36,19 +45,11 @@ impl Card {
             '3' => 3,
             '2' => 2,
             _ => unreachable!(),
-        }
+        })
     }
-}
 
-impl PartialOrd<Self> for Card {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Card {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.value().cmp(&other.value())
+    fn is_joker(&self) -> bool {
+        self.0 == 1
     }
 }
 
@@ -66,84 +67,13 @@ enum HandType {
 #[derive(Eq, PartialEq, Debug)]
 struct Hand {
     cards: [Card; 5],
+    hand_type: HandType,
 }
 
 impl Hand {
-    fn hand_type(&self) -> HandType {
+    fn new(cards: [Card; 5], j_card_type: JCardType) -> Self {
         let sorted_cards = {
-            let mut cards = self.cards;
-            cards.sort();
-            cards
-        };
-
-        let [a, b, c, d, e] = sorted_cards;
-
-        if a == b && b == c && c == d && d == e {
-            HandType::FiveOfAKind
-        } else if (a == b && b == c && c == d) || (b == c && c == d && d == e) {
-            HandType::FourOfAKind
-        } else if (a == b && b == c && d == e) || (a == b && c == d && d == e) {
-            HandType::FullHouse
-        } else if (a == b && b == c) || (b == c && c == d) || (c == d && d == e) {
-            HandType::ThreeOfAKind
-        } else if (a == b && c == d) || (a == b && d == e) || (b == c && d == e) {
-            HandType::TwoPairs
-        } else if a == b || b == c || c == d || d == e {
-            HandType::OnePair
-        } else {
-            HandType::HighCard
-        }
-    }
-}
-
-impl PartialOrd<Self> for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.hand_type()
-            .cmp(&other.hand_type())
-            .reverse()
-            .then(self.cards.cmp(&other.cards))
-    }
-}
-
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-struct JokerCard(char);
-
-impl JokerCard {
-    fn value(&self) -> u32 {
-        match self.0 {
-            'A' => 14,
-            'K' => 13,
-            'Q' => 12,
-            'T' => 10,
-            '9' => 9,
-            '8' => 8,
-            '7' => 7,
-            '6' => 6,
-            '5' => 5,
-            '4' => 4,
-            '3' => 3,
-            '2' => 2,
-            'J' => 1,
-            _ => unreachable!(),
-        }
-    }
-}
-
-#[derive(Eq, PartialEq, Debug)]
-struct JokerHand {
-    cards: [JokerCard; 5],
-}
-
-impl JokerHand {
-    fn hand_type(&self) -> HandType {
-        let sorted_cards = {
-            let mut cards = self.cards;
+            let mut cards = cards;
             cards.sort();
             cards
         };
@@ -166,100 +96,85 @@ impl JokerHand {
             HandType::HighCard
         };
 
-        let num_jokers = sorted_cards.iter().filter(|card| card.0 == 'J').count();
-        match (hand_type, num_jokers) {
-            (HandType::FiveOfAKind, _) => HandType::FiveOfAKind,
-            (HandType::FourOfAKind, 1) | (HandType::FourOfAKind, 4) => HandType::FiveOfAKind,
-            (HandType::FourOfAKind, _) => HandType::FourOfAKind,
-            (HandType::FullHouse, 2) | (HandType::FullHouse, 3) => HandType::FiveOfAKind,
-            (HandType::FullHouse, _) => HandType::FullHouse,
-            (HandType::ThreeOfAKind, 3) => HandType::FiveOfAKind,
-            (HandType::ThreeOfAKind, 1) => HandType::FourOfAKind,
-            (HandType::ThreeOfAKind, _) => HandType::ThreeOfAKind,
-            (HandType::TwoPairs, 2) => HandType::FourOfAKind,
-            (HandType::TwoPairs, 1) => HandType::FullHouse,
-            (HandType::TwoPairs, _) => HandType::TwoPairs,
-            (HandType::OnePair, 2) | (HandType::OnePair, 1) => HandType::ThreeOfAKind,
-            (HandType::OnePair, _) => HandType::OnePair,
-            (HandType::HighCard, 1) => HandType::OnePair,
-            (HandType::HighCard, _) => HandType::HighCard,
-        }
+        let hand_type = match j_card_type {
+            JCardType::Jack => hand_type,
+            JCardType::Joker => {
+                let num_jokers = sorted_cards
+                    .into_iter()
+                    .filter(|card| card.is_joker())
+                    .count();
+
+                match (hand_type, num_jokers) {
+                    (HandType::FiveOfAKind, _) => HandType::FiveOfAKind,
+                    (HandType::FourOfAKind, 1) | (HandType::FourOfAKind, 4) => {
+                        HandType::FiveOfAKind
+                    }
+                    (HandType::FourOfAKind, _) => HandType::FourOfAKind,
+                    (HandType::FullHouse, 2) | (HandType::FullHouse, 3) => HandType::FiveOfAKind,
+                    (HandType::FullHouse, _) => HandType::FullHouse,
+                    (HandType::ThreeOfAKind, 3) => HandType::FiveOfAKind,
+                    (HandType::ThreeOfAKind, 1) => HandType::FourOfAKind,
+                    (HandType::ThreeOfAKind, _) => HandType::ThreeOfAKind,
+                    (HandType::TwoPairs, 2) => HandType::FourOfAKind,
+                    (HandType::TwoPairs, 1) => HandType::FullHouse,
+                    (HandType::TwoPairs, _) => HandType::TwoPairs,
+                    (HandType::OnePair, 2) | (HandType::OnePair, 1) => HandType::ThreeOfAKind,
+                    (HandType::OnePair, _) => HandType::OnePair,
+                    (HandType::HighCard, 1) => HandType::OnePair,
+                    (HandType::HighCard, _) => HandType::HighCard,
+                }
+            }
+        };
+
+        Self { cards, hand_type }
     }
 }
 
-impl PartialOrd<Self> for JokerHand {
+impl PartialOrd<Self> for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl Ord for JokerHand {
+impl Ord for Hand {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.hand_type()
-            .cmp(&other.hand_type())
+        self.hand_type
+            .cmp(&other.hand_type)
             .reverse()
             .then(self.cards.cmp(&other.cards))
     }
 }
 
-impl PartialOrd<Self> for JokerCard {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
+fn solve(input: &str, j_card_type: JCardType) -> Option<u32> {
+    let (_, input) = parse_input(input).unwrap();
+    let mut hands = input
+        .into_iter()
+        .map(|(hand, bid)| {
+            let cards = hand
+                .chars()
+                .map(|c| Card::new(c, j_card_type))
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap();
+            (Hand::new(cards, j_card_type), bid)
+        })
+        .collect::<Vec<_>>();
 
-impl Ord for JokerCard {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.value().cmp(&other.value())
-    }
+    hands.sort();
+
+    hands
+        .into_iter()
+        .enumerate()
+        .map(|(rank, (_, bid))| bid * (rank as u32 + 1))
+        .sum1()
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let (_, input) = parse_input(input).unwrap();
-    let mut hands = input
-        .into_iter()
-        .map(|(hand, bid)| {
-            let cards = hand
-                .chars()
-                .map(Card)
-                .collect::<Vec<_>>()
-                .try_into()
-                .unwrap();
-            (Hand { cards }, bid)
-        })
-        .collect::<Vec<_>>();
-
-    hands.sort();
-
-    hands
-        .into_iter()
-        .enumerate()
-        .map(|(rank, (_, bid))| bid * (rank as u32 + 1))
-        .sum1()
+    solve(input, JCardType::Jack)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let (_, input) = parse_input(input).unwrap();
-    let mut hands = input
-        .into_iter()
-        .map(|(hand, bid)| {
-            let cards = hand
-                .chars()
-                .map(JokerCard)
-                .collect::<Vec<_>>()
-                .try_into()
-                .unwrap();
-            (JokerHand { cards }, bid)
-        })
-        .collect::<Vec<_>>();
-
-    hands.sort();
-
-    hands
-        .into_iter()
-        .enumerate()
-        .map(|(rank, (_, bid))| bid * (rank as u32 + 1))
-        .sum1()
+    solve(input, JCardType::Joker)
 }
 
 #[cfg(test)]
